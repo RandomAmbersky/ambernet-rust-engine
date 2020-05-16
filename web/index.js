062eac663039bda3f786f8f1e8b56740814a0ed8
Random Ambersky <random@ambrskynet.org>
2020-05-16 11:38:11 +0300

    fix

--- web/index.js
+++ web/index.js
@@ -1,69 +1,69 @@
-var VSHADER_SOURCE =
+const VSHADER_SOURCE =
   'void main() {\n' +
-  ' gl_PointSize = 10.0;\n'+
-  ' gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n'+
+  ' gl_PointSize = 10.0;\n' +
+  ' gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' +
   ' }\n'
 
-var FSHADER_SOURCE =
+const FSHADER_SOURCE =
   ' precision mediump float;\n' +
   ' void main() {\n' +
   ' gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);\n' +
-  ' }\n';
+  ' }\n'
 
 function getShader(gl, id, str){
   let shader
   if(id === 'vs'){
-    shader = gl.createShader(gl.VERTEX_SHADER);
+    shader = gl.createShader(gl.VERTEX_SHADER)
   }else if(id==='fs'){
-    shader = gl.createShader(gl.FRAGMENT_SHADER);
+    shader = gl.createShader(gl.FRAGMENT_SHADER)
   }else {
-    return null;
+    return null
   }
 
-  gl.shaderSource(shader, str);
-  gl.compileShader(shader);
+  gl.shaderSource(shader, str)
+  gl.compileShader(shader)
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(gl.getShaderInfoLog(shader));
-    return null;
+    alert(gl.getShaderInfoLog(shader))
+    return null
   }
-  return shader;
+  return shader
 }
 
 function initShaders(gl){
-  var VS = getShader(gl,'vs',VSHADER_SOURCE);
-  var FS = getShader(gl,'fs',FSHADER_SOURCE);
+  const VS = getShader(gl, 'vs', VSHADER_SOURCE)
+  const FS = getShader(gl, 'fs', FSHADER_SOURCE)
 
-  shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram,VS);
-  gl.attachShader(shaderProgram,FS);
-  gl.linkProgram(shaderProgram);
-  gl.useProgram(shaderProgram);
+  shaderProgram = gl.createProgram()
+  gl.attachShader(shaderProgram,VS)
+  gl.attachShader(shaderProgram,FS)
+  gl.linkProgram(shaderProgram)
+  gl.useProgram(shaderProgram)
 }
 
 function webGLStart() {
 
   const canvas = document.getElementById('canvasGL')
   if (!canvas){
-    console.log('failed');
-    return;
+    console.log('failed')
+    return
   }
-  canvas.width =400;
-  canvas.height=400;
+  canvas.width =400
+  canvas.height=400
 
   let gl
   try {
-    gl = canvas.getContext("webgl", {antialias: false});
+    gl = canvas.getContext("webgl", {antialias: false})
   } catch (e) {
-    alert("You are not webgl compatible :(");
-    return false;
+    alert("You are not webgl compatible :(")
+    return false
   }
 
-  initShaders(gl);
+  initShaders(gl)
 
-  gl.clearColor(0.5,0.5,0.5,1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.clearColor(0.5,0.5,0.5,1.0)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.drawArrays(gl.POINTS, 0, 1)
 }
 
-webGLStart();
+webGLStart()
