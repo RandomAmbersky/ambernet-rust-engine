0bcc0f1e5bde405daac445a8957053e934a00086
sir Random <RandomAmbersky@gmail.com>
2020-05-17 15:00:10 +0300

    update

--- web/shaders/vert.glsl
+++ web/shaders/vert.glsl
@@ -1,6 +1,10 @@
+attribute vec2 a_texCoord;
 attribute vec2 a_position;
 
+varying vec2 v_texCoord;
+
 void main() {
-    gl_PointSize = 10.0;
-    gl_Position = vec4(a_position, 0.0, 1.0);
+    gl_PointSize = 30.0;
+    gl_Position = vec4(a_position.x, a_position.y, 0.0, 1.0);
+    v_texCoord = a_texCoord;
 }
