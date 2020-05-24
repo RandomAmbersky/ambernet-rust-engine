9a62b259d6d98b25aaadc5d502b7ee899d0a8b81
Random Ambersky <random@ambrskynet.org>
2020-05-25 00:49:53 +0300

    some ps

--- web/ambersynet/shaders/frag.glsl
+++ web/ambersynet/shaders/frag.glsl
@@ -4,24 +4,37 @@ varying vec2 v_texCoord;
 varying vec3 v_color;
 
 uniform vec2 u_resolution; // screen width x height in pixels
-uniform vec2 u_tile_size;  // tile width x height in pixels
+uniform vec2 u_tile_size;  // one tile width x height in pixels
 uniform vec2 u_map_size; // map width x height in tiles
 
 uniform sampler2D u_image0;
 uniform sampler2D u_image1;
 
 void main() {
+
+    vec2 u_image_size = vec2( 256.,192.);
+    vec2 u_image_scale = u_image_size / u_tile_size;
+
     vec2 st = gl_FragCoord.xy/u_resolution.xy; // [0..1]
+    vec2 mulSt = st*u_map_size; // [0..map_size-1] float
+    vec2 floorMulSt = floor(mulSt); // [0..map_size-1] int
 
-    vec2 mulSt = st*u_map_size;
-    vec2 cellCoordStart = floor(st*u_map_size); // [0..map_size-1]
-    vec2 textureOffset = (mulSt - cellCoordStart) / u_map_size;
+    vec2 textureOffset = mulSt - floorMulSt; // [0..1]
+    textureOffset = textureOffset / u_image_scale;
 
-    vec3 color = vec3(cellCoordStart.x/u_map_size.x,cellCoordStart.y/u_map_size.y,0.); // [0..1]
+//    vec2 cellXY = mulSt / u_map_size; // [0..1]//
+    vec2 cellXYConst = vec2(0., 0.) / u_map_size;
+    vec2 cellXY = cellXYConst;
 
-    vec2 textureCoord = vec2(color.xy)+textureOffset.xy;
+    vec4 cell = texture2D(u_image0, cellXY);
+    vec2 textureCoord = vec2(cell.x, cell.y);
 
-    vec4 textureColor = texture2D(u_image1, textureCoord);
+// ------------
+// some test
+//    vec2 textureCoordConst = vec2(3., 11.) / u_image_scale;
+//    vec2 textureCoord = textureCoordConst;
+// ------------
 
+    vec4 textureColor = texture2D(u_image1, textureCoord + textureOffset);
     gl_FragColor = vec4(textureColor.rgb,1.0);
 }
