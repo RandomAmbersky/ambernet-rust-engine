1e4349377ea5fc29eb15c0d3365cfa96d3fed9c5
Random Ambersky <random@ambrskynet.org>
2021-01-10 17:28:29 +0300

    some updates

--- crate/src/amberskynet/webgl/mod.rs
+++ crate/src/amberskynet/webgl/mod.rs
@@ -1,22 +1,2 @@
 pub mod logger;
 pub mod render;
-
-use super::api;
-use logger::log as webgl_log;
-
-pub struct Logger {}
-
-impl api::LoggerApi for Logger {
-    fn log(&self, mess: &str) {
-        webgl_log(mess)
-    }
-}
-
-pub struct Render {}
-
-impl api::RenderApi for Render {
-    fn new() -> Self {
-        Self {}
-    }
-    fn draw(&self) {}
-}
