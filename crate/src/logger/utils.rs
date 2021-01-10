3638cf46802c4c4e798fd58d059c5dd4c800a6c1
Random Ambersky <random@ambrskynet.org>
2021-01-10 13:29:01 +0300

    some update

--- crate/src/logger/utils.rs
+++
@@ -1,8 +0,0 @@
-    extern crate wasm_bindgen;
-    use wasm_bindgen::prelude::*;
-
-    #[wasm_bindgen]
-    extern "C" {
-        #[wasm_bindgen(js_namespace = console)]
-        pub fn log(s: &str);
-    }
