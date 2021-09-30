afd9be422f3d94c1baab770be7e9885c21e38ae4
polyanin <pa@green-g.ru>
2021-09-30 10:53:58 +0300

    upload textute

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ macos fix wasm
 cd crate
 cargo install wasm-pack 
 ```
+
+```shell
+cd crate
+cargo build --lib --release --target wasm32-unknown-unknown
+```
--- crate/src/amberskynet/render/mod.rs
+++ crate/src/amberskynet/render/mod.rs
@@ -3,7 +3,7 @@ mod test_2d;
 mod binary_font;
 mod texture;
 
-use image::{GenericImageView, ImageResult};
+use image::{GenericImageView};
 use web_sys::WebGlRenderingContext as GL;
 pub use test_2d::Test2D;
 pub use binary_font::BinaryFont;
@@ -61,13 +61,60 @@ pub fn upload_texture(ctx: &RenderContext, bytes: &[u8]) -> Texture {
             panic!("image::load_from_memory error: {}", why)
         }
     };
-    let mut tex = match ctx.gl.create_texture() {
+    let decode_bytes = img.to_rgba8().into_raw();
+    let tex = match ctx.gl.create_texture() {
         Some(t) => t,
         None => {
             panic!("create_texture error")
         }
     };
     ctx.gl.bind_texture(GL::TEXTURE_2D, Some(tex.as_ref()));
+    ctx.gl.pixel_storei(GL::UNPACK_FLIP_Y_WEBGL, 1);
+
+    ctx.gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_S, GL::CLAMP_TO_EDGE as i32);
+    ctx.gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_T, GL::CLAMP_TO_EDGE as i32);
+    ctx.gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::LINEAR as i32);
+    ctx.gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::LINEAR as i32);
+    // ctx.gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::NEAREST as i32);
+    // ctx.gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::NEAREST as i32);
+
+    // target: u32,
+    // level: i32,
+    // internalformat: i32,
+    // width: i32,
+    // height: i32,
+    // border: i32,
+    // format: u32,
+    // type_: u32,
+    // pixels: Option<&[u8]>,
+
+    let level = 0;
+    let internal_format = GL::RGBA;
+    let border = 0;
+    let src_format = GL::RGBA;
+    let src_type = GL::UNSIGNED_BYTE;
+
+    let load_result = match ctx.gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
+        GL::TEXTURE_2D,
+        level,
+        internal_format as i32,
+        img.width() as i32,
+        img.height() as i32,
+        border,
+        src_format,
+        src_type,
+        Some(&decode_bytes),
+    ) {
+        Ok(t) => t,
+        Err(why) => {
+            let err_str = match why.as_string() {
+               Some(t) => t,
+               None => panic!("ctx.gl.tex_image_2d as_string error")
+            };
+            panic!("ctx.gl.tex_image_2d error: {}", &err_str);
+        }
+    };
+
     Texture {
         texture: tex,
         width: img.width(),
