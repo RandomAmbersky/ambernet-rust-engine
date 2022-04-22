b84f1be28b78c9de3559199849dd5b7d463506aa
sir Random <RandomAmbersky@gmail.com>
2022-04-22 10:57:21 +0300

    useless updates

--- web/index.js
+++ web/index.js
@@ -1,15 +1,15 @@
 import module from '../src/amberskynet/Cargo.toml'
 
-const FPS_THROTTLE = 1000.0 / 30.0; // milliseconds / frames
+const FPS_THROTTLE = 1000.0 / 30.0 // milliseconds / frames
 
-const canvas = document.getElementById('canvasGL');
+const canvas = document.getElementById('canvasGL')
 const engine = new module.AmberSkyNetClient()
 
 let lastDrawTime = Date.now();// In milliseconds
 
 const update = (currTime) => {
-  let elapsedTime = currTime - lastDrawTime;
-  lastDrawTime = currTime;
+  let elapsedTime = currTime - lastDrawTime
+  lastDrawTime = currTime
 
   if (window.innerHeight !== canvas.height || window.innerWidth !== canvas.width) {
     canvas.height = window.innerHeight
