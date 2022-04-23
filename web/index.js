e4c031f4206b160675a2a89c08ddccfab2142c4d
sir Random <RandomAmbersky@gmail.com>
2022-04-23 13:00:29 +0300

    upload tiles and map to rust engine

--- web/index.js
+++ web/index.js
@@ -5,6 +5,50 @@ const FPS_THROTTLE = 1000.0 / 30.0 // milliseconds / frames
 const canvas = document.getElementById('canvasGL')
 const engine = new module.AmberSkyNetClient()
 
+async function load (uri, responseType = "text") {
+  return new Promise( function (resolve, reject){
+    const xhr = new XMLHttpRequest()
+    xhr.open("GET", uri, true)
+    xhr.responseType = responseType;
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response)
+      } else {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText
+        })
+      }
+    }
+    xhr.onerror = function () {
+      reject({
+        status: xhr.status,
+        statusText: xhr.statusText
+      })
+    }
+    xhr.send()
+  })
+}
+
+async function loadData () {
+  const tiles_buf = await load('/map/tiles_many.png')
+  const map_buf = await load('/map/laboratory3.json')
+
+  const mapArray = new Uint8Array(map_buf.response)
+  const tilesArray = new Uint8Array(tiles_buf.response)
+
+  engine.upload_tiles(tilesArray)
+  engine.upload_map(mapArray)
+}
+
+loadData()
+  .then( _ => {
+    renderLoop()
+  })
+  .catch(err => {
+    alert(err)
+  })
+
 let lastDrawTime = Date.now();// In milliseconds
 
 const update = (currTime) => {
@@ -28,5 +72,3 @@ function renderLoop() {
   }
   window.requestAnimationFrame(renderLoop);
 }
-
-renderLoop()
