f4ee953d8e82f05f70586bf9f0d26ca200bfb0c0
sir Random <RandomAmbersky@gmail.com>
2022-04-27 11:44:21 +0300

    it worked

---
+++ src/asn_render_color_quad/src/lib.rs
@@ -0,0 +1,99 @@
+use web_sys::{WebGlBuffer, WebGlProgram};
+use asn_render_webgl::RenderContext;
+use web_sys::WebGlRenderingContext as GL;
+
+pub struct ColorQuad {
+	program: WebGlProgram,
+	vertices_buf: WebGlBuffer,
+	indices_buf: WebGlBuffer,
+	colors_buf: WebGlBuffer,
+	indices_len: i32
+}
+
+pub fn new_item (
+	ctx: &RenderContext
+) -> ColorQuad {
+
+	let vertices: [f32; 12] = [
+		-0.5, 0.5, 0.0,
+		-0.5, -0.5, 0.0,
+		0.5, -0.5, 0.0,
+		0.5, 0.5, 0.0,
+	];
+	let vertices_buf = asn_render_webgl::load_buffer(&ctx, &vertices);
+
+	let indices: [u16; 6] = [3, 2, 1, 3, 1, 0];
+	let indices_buf = asn_render_webgl::load_index_buffer(&ctx, &indices);
+
+	let colors: [f32; 12] = [
+		0.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 0.0, 1.0
+	];
+	let colors_buf = asn_render_webgl::load_buffer(&ctx, &colors);
+
+	let vert_code = r#"attribute vec3 coordinates;
+attribute vec3 color;
+varying vec3 vColor;
+void main(void) {
+   gl_Position = vec4(coordinates, 1.0);
+   vColor = color;
+}
+"#;
+
+	let frag_code = r#"precision mediump float;
+varying vec3 vColor;
+void main(void) {
+    gl_FragColor = vec4(vColor, 1.);
+}"#;
+	let program = asn_render_webgl::link_program(&ctx, vert_code, frag_code).unwrap();
+
+	ColorQuad {
+		program,
+		vertices_buf,
+		indices_buf,
+		colors_buf,
+		indices_len: indices.len() as i32
+	}
+}
+
+pub fn draw(ctx: &RenderContext, item: &ColorQuad) {
+	// Bind vertex buffer object
+	ctx.gl.use_program(Some(&item.program));
+
+	ctx.gl.bind_buffer( GL::ARRAY_BUFFER, Some(&item.vertices_buf));
+	ctx.gl.bind_buffer( GL::ELEMENT_ARRAY_BUFFER, Some(&item.indices_buf));
+
+	// Get the attribute location
+	let coord = ctx.gl.get_attrib_location(&item.program, "coordinates") as u32;
+	// Point an attribute to the currently bound VBO
+	ctx.gl.vertex_attrib_pointer_with_i32(coord, 3, GL::FLOAT, false, 0, 0);
+	// Enable the attribute
+	ctx.gl.enable_vertex_attrib_array(coord);
+
+	// bind the color buffer
+	ctx.gl.bind_buffer(GL::ARRAY_BUFFER, Some(&item.colors_buf));
+	// get the attribute location
+	let color = ctx.gl.get_attrib_location(&item.program, "color") as u32;
+	// point attribute to the volor buffer object
+	ctx.gl.vertex_attrib_pointer_with_i32(color, 3, GL::FLOAT, false, 0, 0);
+	// enable the color attribute
+	ctx.gl.enable_vertex_attrib_array(color);
+
+	// Clear the canvas
+	ctx.gl.clear_color(0.5, 0.5, 0.5, 1.0);
+
+	// Enable the depth test
+	ctx.gl.enable(GL::DEPTH_TEST);
+
+	// Clear the color buffer bit
+	ctx.gl.clear(GL::COLOR_BUFFER_BIT);
+
+	ctx.gl.draw_elements_with_i32(
+		GL::TRIANGLES,
+		item.indices_len,
+		GL::UNSIGNED_SHORT,
+		0,
+	);
+}
