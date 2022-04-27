25e638e25e64b176b96fe5e74e68e0354d128c1a
sir Random <RandomAmbersky@gmail.com>
2022-04-27 22:01:18 +0300

    little refs

---
+++ src/asn_render_color_quad/src/utils.rs
@@ -0,0 +1,28 @@
+pub const VERTICES: [f32; 12] = [
+-0.5, 0.5, 0.0,
+-0.5, -0.5, 0.0,
+0.5, -0.5, 0.0,
+0.5, 0.5, 0.0,
+];
+
+pub const COLORS: [f32; 12] = [
+0.0, 0.0, 0.0,
+1.0, 0.0, 0.0,
+0.0, 1.0, 0.0,
+0.0, 0.0, 1.0
+];
+
+pub const VERTEX_SHADER: &str = r#"attribute vec3 coordinates;
+attribute vec3 color;
+varying vec3 vColor;
+void main(void) {
+   gl_Position = vec4(coordinates, 1.0);
+   vColor = color;
+}
+"#;
+
+pub const FRAG_SHADER: &str = r#"precision mediump float;
+varying vec3 vColor;
+void main(void) {
+    gl_FragColor = vec4(vColor, 1.);
+}"#;
