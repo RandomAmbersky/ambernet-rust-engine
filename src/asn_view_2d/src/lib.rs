1180365410c72bd2c32eac6cfe92cb820866ee09
sir Random <RandomAmbersky@gmail.com>
2022-04-27 10:56:56 +0300

    temporary fixes

--- src/asn_view_2d/src/lib.rs
+++ src/asn_view_2d/src/lib.rs
@@ -8,15 +8,16 @@ use asn_render_webgl::RenderContext;
 pub struct View2D {
 	program: WebGlProgram,
 	buffer: WebGlBuffer,
-	w: i32,
-	h: i32
+	w_cells: i32,
+	h_cells: i32
 }
 
-pub fn new_item (ctx: &RenderContext, w: i32, h: i32) -> View2D {
+pub fn new_item (ctx: &RenderContext, w_cells: i32, h_cells: i32) -> View2D {
 	let program = asn_render_webgl::link_program(ctx, shaders::VERT_SHADER, shaders::FRAG_SHADER).unwrap();
 	let buffer = asn_render_webgl::load_buffer(ctx, &buffer::SIMPLE_BUF);
+
 	View2D {
-		w, h,
+		w_cells, h_cells,
 		program,
 		buffer
 	}
@@ -24,6 +25,17 @@ pub fn new_item (ctx: &RenderContext, w: i32, h: i32) -> View2D {
 
 pub fn draw (ctx: &RenderContext, item: &View2D) {
 	let gl = &ctx.gl;
+	gl.clear(GL::COLOR_BUFFER_BIT);
 	gl.use_program(Some(&item.program));
+
+	let a_position: u32 = ctx.gl.get_attrib_location(&item.program, "a_Position") as u32;
+	ctx.gl.vertex_attrib_pointer_with_i32(a_position, 3, GL::FLOAT, false, 0, 0);
+	ctx.gl.enable_vertex_attrib_array(3);
+
+	// let min_size = gl.get_parameter(GL::ALIASED_POINT_SIZE_RANGE);
+	// let a_Position: u32 = gl.get_attrib_location(&item.program, "a_Position");
+	// gl.vertex_attrib_pointer_with_float_dimension(a_Position, 3., GL::FLOAT, false, 0, 0);
+
 	gl.bind_buffer(GL::ARRAY_BUFFER, Some(&item.buffer));
+	gl.draw_arrays(GL::TRIANGLES, 0, 6);
 }
--- src/asn_view_2d/src/shaders.rs
+++ src/asn_view_2d/src/shaders.rs
@@ -1,18 +1,29 @@
 
 pub const FRAG_SHADER: &str = "
-        precision mediump float;
-
-        uniform vec4 uColor;
-        uniform float uOpacity;
-
         void main() {
-            gl_FragColor = vec4(uColor.r, uColor.g, uColor.b, uColor.a * uOpacity);
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
         }";
 
-pub const VERT_SHADER: &str = r#"
-        attribute vec4 aPosition;
-        uniform mat4 uTransform;
+// pub const FRAG_SHADER: &str = "
+//         precision mediump float;
+//
+//         uniform vec4 uColor;
+//         uniform float uOpacity;
+//
+//         void main() {
+//             gl_FragColor = vec4(uColor.r, uColor.g, uColor.b, uColor.a * uOpacity);
+//         }";
 
+pub const VERT_SHADER: &str = r#"
+				attribute vec4 a_Position;
         void main() {
-            gl_Position = uTransform * aPosition;
+            gl_Position = a_Position;
         }"#;
+
+// pub const VERT_SHADER: &str = r#"
+//         attribute vec4 aPosition;
+//         uniform mat4 uTransform;
+//
+//         void main() {
+//             gl_Position = uTransform * aPosition;
+//         }"#;
