d9d363d18218db3611b9c8254d99614afe7b1cc4
sir Random <RandomAmbersky@gmail.com>
2022-08-04 11:39:58 +0300

    textures refactoring

--- src/asn_render_webgl/src/textures/mod.rs
+++ src/asn_render_webgl/src/textures/mod.rs
@@ -63,44 +63,7 @@ pub fn upload_raw_texture(gl: &GL, bytes: &Vec<u8>, width: i32, height: i32) -> 
 			panic!("create_texture error")
 		}
 	};
-	gl.bind_texture(GL::TEXTURE_2D, Some(texture.as_ref()));
-	gl.pixel_storei(GL::UNPACK_FLIP_Y_WEBGL, 1);
-
-	gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_S, GL::CLAMP_TO_EDGE as i32);
-	gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_T, GL::CLAMP_TO_EDGE as i32);
-	gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::LINEAR as i32);
-	gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::LINEAR as i32);
-	gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::NEAREST as i32);
-	gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::NEAREST as i32);
-
-	let level = 0;
-	let internal_format = GL::RGBA;
-	let border = 0;
-	let src_format = GL::RGBA;
-	let src_type = GL::UNSIGNED_BYTE;
-
-	match gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
-		GL::TEXTURE_2D,
-		level,
-		internal_format as i32,
-		width,
-		height,
-		border,
-		src_format,
-		src_type,
-		Some(&bytes),
-	) {
-		Ok(t) => t,
-		Err(why) => {
-			let err_str = match why.as_string() {
-				Some(t) => t,
-				None => panic!("gl.tex_image_2d as_string error")
-			};
-			panic!("gl.tex_image_2d error: {}", &err_str);
-		}
-	};
-
-	gl.bind_texture(GL::TEXTURE_2D, None);
+	update_raw_texture(gl, Some(&texture), bytes, width, height);
 	texture
 }
 
