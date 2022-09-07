b21d8d0304c3798f38951c9f15cd8788953e7b90
sir Random <RandomAmbersky@gmail.com>
2022-09-07 23:46:11 +0300

    little upgrade

--- src/amberskynet/src/cell_game.rs
+++ src/amberskynet/src/cell_game.rs
@@ -5,8 +5,8 @@ use asn_view_2d::View2D;
 use crate::new_view_2d;
 
 const WINDOW_SIZE: Size2D = Size2D {
-	width: 10,
-	height: 10
+	width: 32,
+	height: 32
 };
 
 pub struct CellGame {
@@ -35,9 +35,8 @@ impl CellGame {
 		Ok(())
 	}
 
-	pub fn update(&mut self, _time: f32) -> Result<(), String> {
-		// self.view.set_view(&self.window_size, &self.map)?;
-		// self.view.update(time)?;
+	pub fn update(&mut self, time: f32) -> Result<(), String> {
+		self.view.update(time)?;
 		Ok(())
 	}
 
