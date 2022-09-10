978cc766db04019c0e99de0902f49f1cdb337058
sir Random <RandomAmbersky@gmail.com>
2022-09-10 11:42:07 +0300

    some refactoring

--- src/amberskynet/src/cell_game.rs
+++ src/amberskynet/src/cell_game.rs
@@ -1,35 +1,40 @@
-use asn_core::Array2D;
+use asn_core::{Array2D, Camera2D};
 use asn_core::{Point2D, Size2D};
 use asn_render_webgl::RenderContext;
 use asn_view_2d::View2D;
 use crate::new_view_2d;
 
 const WINDOW_SIZE: Size2D = Size2D {
-	width: 10,
-	height: 10
+	width: 20,
+	height: 20
 };
 
 pub struct CellGame {
 	pub view: View2D,
 	pub map: Array2D,
-	window_size: Size2D
+	pub cam: Camera2D
 }
 
 impl CellGame {
 	pub fn new(ctx: &RenderContext) -> Result<CellGame, String> {
 		let view_2d = new_view_2d(&ctx)?;
 
+		let cam = Camera2D {
+			pos: Default::default(),
+			window: WINDOW_SIZE
+		};
+
 		let game = CellGame {
 			map: Default::default(),
-			window_size: WINDOW_SIZE,
-			view: view_2d
+			view: view_2d,
+			cam,
 		};
 		Ok(game)
 	}
 
 	pub fn set_map(&mut self, map: Array2D) -> Result<(), String> {
 		self.map = map;
-		self.view.set_view(&self.window_size, &self.map)?;
+		self.view.look_at(&mut self.cam, &self.map)?;
 		Ok(())
 	}
 
@@ -39,7 +44,8 @@ impl CellGame {
 	}
 
 	pub fn look_at(&mut self, pos: &Point2D) -> Result<(), String> {
-		self.view.look_at(pos, &self.window_size, &self.map)?;
+		self.cam.pos = *pos;
+		self.view.look_at(&mut self.cam, &self.map)?;
 		Ok(())
 	}
 }
--- src/asn_view_2d/src/lib.rs
+++ src/asn_view_2d/src/lib.rs
@@ -1,17 +1,15 @@
 mod utils;
 mod texture_data;
 mod render_data;
-mod view_utils;
 
 use amberskynet_logger_web::LoggerWeb;
 use asn_render_webgl::{ RenderContext };
 
 use web_sys::WebGlRenderingContext as GL;
-use asn_core::{Array2D, Point2D, Size2D};
+use asn_core::{Array2D, Camera2D, Size2D};
 use asn_core::math::mult_matrix_4;
 
 use render_data::RenderData;
-use crate::view_utils::{look_at, set_view_from};
 
 pub struct View2D {
 	view: Array2D,
@@ -66,22 +64,8 @@ pub fn get_index (item: &mut View2D, x: u32, y: u32) -> Result<usize, String> {
 }
 
 impl View2D {
-	pub fn set_view (&mut self, window: &Size2D, map: &Array2D) -> Result<(), String> {
-		if window.is_zero() {
-			return Err(String::from("window size is zero"))
-		}
-
-		let pos: Point2D = Point2D {
-			x: 0,
-			y: 0
-		};
-		set_view_from(map, &pos, window, &mut self.view)?;
-		self.is_need_update_texture_view = true;
-		Ok(())
-	}
-
-	pub fn look_at (&mut self, pos: &Point2D, window: &Size2D, map: &Array2D) -> Result<(), String> {
-		look_at(map, pos, window, &mut self.view)?;
+	pub fn look_at (&mut self, cam: &mut Camera2D, map: &Array2D) -> Result<(), String> {
+		cam.look_at(map, &mut self.view)?;
 		self.is_need_update_texture_view = true;
 		Ok(())
 	}
