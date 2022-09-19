21bf8c1c7239449ab607efd2c7996f891a1cf3d3
sir Random <RandomAmbersky@gmail.com>
2022-09-19 08:22:21 +0300

    prepare to parse tiled ecs

--- src/amberskynet/src/logic/mod.rs
+++ src/amberskynet/src/logic/mod.rs
@@ -80,8 +80,11 @@ impl Logic {
         Ok(())
     }
 
-    pub fn set_map(&mut self, w: &mut World, map: Array2D) -> Result<(), String> {
+    pub fn set_background(&mut self, w: &mut World, map: Array2D) -> Result<(), String> {
+        Ok(())
+    }
 
+    pub fn set_map(&mut self, w: &mut World, map: Array2D) -> Result<(), String> {
         for i in 0..256 {
             match self.get_tile_type(&i) {
                 Ok(t) => {
