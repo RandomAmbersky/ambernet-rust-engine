63be6d1434552b32c58dde7d570733f897b502f3
sir Random <RandomAmbersky@gmail.com>
2022-10-07 23:20:42 +0300

    fix some strange

--- rs-amberskynet/src/lib.rs
+++ rs-amberskynet/src/lib.rs
@@ -1,5 +1,6 @@
 mod vertex;
 mod texture;
+mod resource;
 
 use std::iter;
 use wgpu::CompositeAlphaMode;
@@ -12,34 +13,10 @@ use winit::{
 };
 
 use vertex::Vertex;
+use resource::{VERTICES, INDICES};
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
-
-const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
-        tex_coords: [0.4131759, 0.00759614],
-    }, // A
-    Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
-        tex_coords: [0.0048659444, 0.43041354],
-    }, // B
-    Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
-        tex_coords: [0.28081453, 0.949397],
-    }, // C
-    Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
-        tex_coords: [0.85967, 0.84732914],
-    }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        tex_coords: [0.9414737, 0.2652641],
-    }, // E
-];
-
-const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
 
 struct State {
     surface: wgpu::Surface,
