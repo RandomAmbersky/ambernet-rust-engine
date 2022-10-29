190ec71e614a6b83772ac8c2da92d5e51e0cb04f
sir Random <RandomAmbersky@gmail.com>
2022-10-29 11:32:34 +0300

    temporary fixed

--- rs-amberskynet/src/engine/mod.rs
+++ rs-amberskynet/src/engine/mod.rs
@@ -1,21 +1,62 @@
-use winit::event::Event;
+use log::{debug, error};
+use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget};
-use winit::window::{Window, WindowBuilder};
+use winit::window::{Window, WindowBuilder, WindowId};
 
 pub struct AsnEngine {
-	window: Window
+    window: Window,
 }
 
 impl AsnEngine {
-	pub fn new(event_loop: &EventLoop<()>) -> Self {
-		let window = WindowBuilder::new().build(&event_loop).unwrap();
-		AsnEngine {
-			window
-		}
-	}
+    pub fn new(event_loop: &EventLoop<()>) -> Self {
+        let window = WindowBuilder::new().build(&event_loop).unwrap();
+        AsnEngine { window }
+    }
 
-	pub fn process_event(&mut self, event: &Event<()>, event_loop_window_target: &EventLoopWindowTarget<()>, control_flow: &mut ControlFlow) {
-		match event {
-		}
-	}
+    pub fn process_event(
+        &mut self,
+        event: &Event<()>,
+        event_loop_window_target: &EventLoopWindowTarget<()>,
+        control_flow: &mut ControlFlow,
+    ) {
+        // debug!("this is a event {:?}", event);
+        match event {
+            Event::WindowEvent { window_id, event } => {
+                return self.process_window_event(control_flow, window_id, event)
+            }
+            Event::RedrawRequested(window_id) => return self.process_redraw_requested(window_id),
+            Event::MainEventsCleared {} => return self.process_main_events_cleared(),
+            _ => {}
+        }
+    }
+}
+
+impl AsnEngine {
+    fn process_window_event(
+        &mut self,
+        control_flow: &mut ControlFlow,
+        window_id: &WindowId,
+        event: &WindowEvent,
+    ) {
+        if window_id != &self.window.id() {
+            error!("not correct window_id: {:?}", window_id);
+            return;
+        }
+        debug!("this is a debug {:?}", event);
+    }
+}
+
+impl AsnEngine {
+    fn process_redraw_requested(&mut self, window_id: &WindowId) {
+        if window_id != &self.window.id() {
+            error!("not correct window_id: {:?}", window_id);
+            return;
+        }
+    }
+}
+
+impl AsnEngine {
+    fn process_main_events_cleared(&mut self) {
+        self.window.request_redraw();
+    }
 }
--- rs-amberskynet/src/lib.rs
+++ rs-amberskynet/src/lib.rs
@@ -1,24 +1,23 @@
-mod vertex;
-mod texture;
-mod state;
-mod view_2d;
-mod renderer;
 mod engine;
+mod renderer;
+mod state;
+mod texture;
+mod vertex;
+mod view_2d;
 
+use std::env;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
+use crate::engine::AsnEngine;
 use vertex::Vertex;
 use view_2d::resource::{INDICES, VERTICES};
-use crate::engine::AsnEngine;
-use crate::renderer::AsnRenderer;
-
-use crate::state::State;
 
 pub async fn run() {
+    env::set_var("RUST_LOG", "trace");
     env_logger::init();
 
     let event_loop = EventLoop::new();
@@ -34,54 +33,54 @@ pub async fn run() {
 
     // event_loop.run(move |event, _, control_flow| {
     //     renderer.process_event(&event, control_flow)
-        // match event {
-        //     Event::WindowEvent {
-        //         ref event,
-        //         window_id,
-        //     } if window_id == window.id() => {
-        //         if !state.input(event) {
-        //             match event {
-        //                 WindowEvent::CloseRequested
-        //                 | WindowEvent::KeyboardInput {
-        //                     input:
-        //                         KeyboardInput {
-        //                             state: ElementState::Pressed,
-        //                             virtual_keycode: Some(VirtualKeyCode::Escape),
-        //                             ..
-        //                         },
-        //                     ..
-        //                 } => *control_flow = ControlFlow::Exit,
-        //                 WindowEvent::Resized(physical_size) => {
-        //                     state.resize(*physical_size);
-        //                 }
-        //                 WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-        //                     // new_inner_size is &mut so w have to dereference it twice
-        //                     state.resize(**new_inner_size);
-        //                 }
-        //                 _ => {}
-        //             }
-        //         }
-        //     }
-        //     Event::RedrawRequested(window_id) if window_id == window.id() => {
-        //         state.update();
-        //         match state.render() {
-        //             Ok(_) => {}
-        //             // Reconfigure the surface if it's lost or outdated
-        //             Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-        //                 state.reload_size()
-        //             }
-        //             // The system is out of memory, we should probably quit
-        //             Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-        //             // We're ignoring timeouts
-        //             Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
-        //         }
-        //     }
-        //     Event::MainEventsCleared => {
-        //         // RedrawRequested will only trigger once, unless we manually
-        //         // request it.
-        //         window.request_redraw();
-        //     }
-        //     _ => {}
-        // }
+    // match event {
+    //     Event::WindowEvent {
+    //         ref event,
+    //         window_id,
+    //     } if window_id == window.id() => {
+    //         if !state.input(event) {
+    //             match event {
+    //                 WindowEvent::CloseRequested
+    //                 | WindowEvent::KeyboardInput {
+    //                     input:
+    //                         KeyboardInput {
+    //                             state: ElementState::Pressed,
+    //                             virtual_keycode: Some(VirtualKeyCode::Escape),
+    //                             ..
+    //                         },
+    //                     ..
+    //                 } => *control_flow = ControlFlow::Exit,
+    //                 WindowEvent::Resized(physical_size) => {
+    //                     state.resize(*physical_size);
+    //                 }
+    //                 WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+    //                     new_inner_size is &mut so w have to dereference it twice
+    // state.resize(**new_inner_size);
+    // }
+    // _ => {}
+    // }
+    // }
+    //     }
+    //     Event::RedrawRequested(window_id) if window_id == window.id() => {
+    //         state.update();
+    //         match state.render() {
+    //             Ok(_) => {}
+    //             // Reconfigure the surface if it's lost or outdated
+    //             Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+    //                 state.reload_size()
+    //             }
+    //             // The system is out of memory, we should probably quit
+    //             Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+    //             // We're ignoring timeouts
+    //             Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+    //         }
+    //     }
+    //     Event::MainEventsCleared => {
+    //         // RedrawRequested will only trigger once, unless we manually
+    //         // request it.
+    //         window.request_redraw();
+    //     }
+    //     _ => {}
+    // }
     // });
 }
