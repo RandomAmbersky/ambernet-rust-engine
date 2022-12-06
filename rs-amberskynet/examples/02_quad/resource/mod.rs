6ea1b6ba327eb02777f1a605c8fb0dfdb399be53
sir Random <RandomAmbersky@gmail.com>
2022-12-06 21:19:39 +0300

    temporary fixed

--- rs-amberskynet/examples/02_quad/resource/mod.rs
+++ rs-amberskynet/examples/02_quad/resource/mod.rs
@@ -2,14 +2,13 @@ use rs_amberskynet::gfx::Vertex;
 
 pub const TEXTURE_SOURCE: &[u8] = include_bytes!("tiles_mod.png");
 pub const SHADER_SOURCE: &str = include_str!("shader.wgsl");
-
 pub const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
+        position: [0.0, 0.5, 0.0],
         tex_coords: [0.4131759, 0.00759614],
     }, // A
     Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
+        position: [0.0, 0.0, 0.0],
         tex_coords: [0.0048659444, 0.43041354],
     }, // B
     Vertex {
@@ -21,9 +20,11 @@ pub const VERTICES: &[Vertex] = &[
         tex_coords: [0.85967, 0.84732914],
     }, // D
     Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
+        position: [0.5, 0.5, 0.0],
         tex_coords: [0.9414737, 0.2652641],
     }, // E
 ];
 
-pub const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, 0];
+pub const INDICES: &[u16] = &[0, 1, 4];
+
+// pub const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, 0];
