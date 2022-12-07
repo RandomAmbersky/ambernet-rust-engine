99cfed7fe0d938a002f47b1c22b087f3c2625812
sir Random <RandomAmbersky@gmail.com>
2022-12-07 10:45:38 +0300

    temporary fixed

--- rs-amberskynet/examples/02_quad/resource/mod.rs
+++ rs-amberskynet/examples/02_quad/resource/mod.rs
@@ -4,19 +4,21 @@ pub const TEXTURE_SOURCE: &[u8] = include_bytes!("tiles_mod.png");
 pub const SHADER_SOURCE: &str = include_str!("shader.wgsl");
 pub const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        tex_coords: [0.4131759, 0.00759614],
-    }, // A
-    Vertex {
-        position: [0.0, 0.0, 0.0],
-        tex_coords: [0.0048659444, 0.43041354],
+        position: [-1.0, -1.0, 0.0],
+        tex_coords: [0.0, 0.0],
     }, // B
     Vertex {
-        position: [0.5, 0.5, 0.0],
-        tex_coords: [0.9414737, 0.2652641],
+        position: [1.0, 1.0, 0.0],
+        tex_coords: [1.0, 1.0],
     }, // E
+    Vertex {
+        position: [-1.0, 1.0, 0.0],
+        tex_coords: [0.0, 1.0],
+    }, // A
+    Vertex {
+        position: [1.0, -1.0, 0.0],
+        tex_coords: [1.0, 0.0],
+    }, // A
 ];
 
-pub const INDICES: &[u16] = &[0, 1, 2];
-
-// pub const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, 0];
+pub const INDICES: &[u16] = &[0, 1, 2, 0, 3, 1];
