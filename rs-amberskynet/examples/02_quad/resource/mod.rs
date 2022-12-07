6189aeaed9015f02485dafe70151d26f7220c6fa
sir Random <RandomAmbersky@gmail.com>
2022-12-07 19:21:23 +0300

    temporary fixed

--- rs-amberskynet/examples/02_quad/resource/mod.rs
+++ rs-amberskynet/examples/02_quad/resource/mod.rs
@@ -5,19 +5,19 @@ pub const SHADER_SOURCE: &str = include_str!("shader.wgsl");
 pub const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-1.0, -1.0, 0.0],
-        tex_coords: [0.0, 0.0],
+        tex_coords: [0.0, 1.0],
     }, // B
     Vertex {
         position: [1.0, 1.0, 0.0],
-        tex_coords: [1.0, 1.0],
+        tex_coords: [1.0, 0.0],
     }, // E
     Vertex {
         position: [-1.0, 1.0, 0.0],
-        tex_coords: [0.0, 1.0],
+        tex_coords: [0.0, 0.0],
     }, // A
     Vertex {
         position: [1.0, -1.0, 0.0],
-        tex_coords: [1.0, 0.0],
+        tex_coords: [1.0, 1.0],
     }, // A
 ];
 
