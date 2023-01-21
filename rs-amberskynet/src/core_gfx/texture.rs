b29acd56751b9a02c6a39ccf792d03240e937c24
sir Random <RandomAmbersky@gmail.com>
2023-01-21 11:57:19 +0300

    temporary fixed

--- rs-amberskynet/src/core_gfx/texture.rs
+++ rs-amberskynet/src/core_gfx/texture.rs
@@ -1,7 +1,5 @@
 use crate::core::{Array2D, AsnError, Size2D};
 use crate::core_gfx::gfx_context::GfxContextTrait;
-use crate::core_gfx::pixel::PixelRGBA;
-use crate::core_gfx::point2d::Point2D;
 
 pub trait AsnTextureTrait<T, G, E>
 where
@@ -15,9 +13,4 @@ where
     fn update_from_array(&mut self, gfx: &G, array: &Array2D<u32, u8>) -> Result<(), E>;
 
     fn get_size(&self) -> Size2D<u32>;
-}
-
-pub trait AsnSurfaceTrait<U, S> {
-    fn get_size(&self) -> Size2D<u32>;
-    fn set_pixel(&mut self, pos: Point2D<U>, p: &PixelRGBA<S>);
 }
