828d4f2c683ccb1c7984c863c6b39bb06e54fa16
sir Random <RandomAmbersky@gmail.com>
2023-02-12 18:48:47 +0300

    temporary fixed

--- rs-amberskynet/src/core/pos2d.rs
+++ rs-amberskynet/src/core/pos2d.rs
@@ -1,10 +1,27 @@
-use std::fmt::Display;
-
-#[derive(Default, Debug, Copy, Clone, PartialOrd, PartialEq)]
-pub struct Pos2D<S>
-where
-    S: Display,
-{
+pub struct Pos2D<S> {
     pub x: S,
     pub y: S,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::pos2d::Pos2D;
+
+    #[test]
+    fn it_works() {
+        let value: u32 = 10;
+        let pos = Pos2D { x: value, y: value };
+        assert_eq!(pos.x, value);
+        assert_eq!(pos.y, value);
+    }
+
+    #[test]
+    fn it_eq() {
+        let value: u32 = 10;
+        let pos = Pos2D { x: value, y: value };
+
+        let pos2 = pos;
+        assert_eq!(pos2.x, value);
+        assert_eq!(pos2.y, value);
+    }
 }
--- rs-amberskynet/src/core/size2d.rs
+++ rs-amberskynet/src/core/size2d.rs
@@ -1,24 +1,35 @@
-use crate::core::Pos2D;
-use std::fmt::{Debug, Display};
+use crate::core::pos2d::Pos2D;
 use std::ops::{Add, Mul};
 
-#[derive(Default, Debug, Copy, Clone, PartialOrd, PartialEq)]
-pub struct Size2D<T>
-where
-    T: Debug,
-{
+pub struct Size2D<T> {
     pub width: T,
     pub height: T,
 }
 
 impl<T> Size2D<T>
 where
-    T: Debug + Display + Mul<T, Output = T> + Add<T, Output = T> + Copy,
+    T: Mul<T, Output = T> + Add<T, Output = T> + Copy,
 {
-    pub fn get_index_from_xy(self, x: T, y: T) -> T {
+    pub fn get_index_from_xy(&self, x: T, y: T) -> T {
         self.width * y + x
     }
-    pub fn get_index(self, pos: &Pos2D<T>) -> T {
+    pub fn get_index(&self, pos: &Pos2D<T>) -> T {
         self.width * pos.y + pos.x
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::size2d::Size2D;
+
+    #[test]
+    fn it_works() {
+        let value: u32 = 10;
+        let pos = Size2D {
+            width: value,
+            height: value,
+        };
+        assert_eq!(pos.width, value);
+        assert_eq!(pos.height, value);
     }
 }
