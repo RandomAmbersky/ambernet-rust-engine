9ea984fd86e6f828df4fc7d650273dfbd3ef3b38
sir Random <RandomAmbersky@gmail.com>
2023-02-13 11:49:47 +0300

    temporary fixed

---
+++ rs-core/src/lib.rs
@@ -0,0 +1,20 @@
+mod array2d;
+mod axe_dimension;
+mod error;
+mod pos2d;
+mod size2d;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
