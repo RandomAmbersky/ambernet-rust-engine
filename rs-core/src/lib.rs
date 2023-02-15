42bfbeb5263db07b6f49c5f1ef0d82af21d59b4a
sir Random <RandomAmbersky@gmail.com>
2023-02-15 21:06:15 +0300

    temporary fixed

--- rs-core/src/lib.rs
+++ rs-core/src/lib.rs
@@ -3,3 +3,10 @@ mod cell_type;
 mod pos2d;
 mod size2d;
 mod unsigned_num;
+
+pub use cell_type::CellType;
+pub use unsigned_num::UnsignedNum;
+
+pub use array2d::Array2D;
+pub use pos2d::Pos2D;
+pub use size2d::Size2D;
