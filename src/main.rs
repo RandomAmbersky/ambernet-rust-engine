53de397165771f148e5b20e1fca8a0af78c6312d
sir Random <RandomAmbersky@gmail.com>
2023-05-20 06:19:53 +0300

    some updates

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,80 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::render::WindowCanvas;
 use sdl2::EventPump;
+use std::time::Duration;
+
+pub enum GameState {
+    Playing,
+    Paused,
+}
+pub enum PlayerDirection {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+pub struct Point(pub i32, pub i32);
+
+pub struct GameContext {
+    pub player_position: Vec<Point>,
+    pub player_direction: PlayerDirection,
+    pub food: Point,
+    pub state: GameState,
+}
+
+impl Default for GameContext {
+    fn default() -> Self {
+        GameContext {
+            player_position: vec![Point(3, 1), Point(2, 1), Point(1, 1)],
+            player_direction: PlayerDirection::Right,
+            state: GameState::Paused,
+            food: Point(3, 3),
+        }
+    }
+}
 
 struct MainData {
     event_pump: EventPump,
     canvas: WindowCanvas,
+    c: GameContext,
 }
+
+const GRID_X_SIZE: u32 = 40;
+const GRID_Y_SIZE: u32 = 30;
+const DOT_SIZE_IN_PXS: u32 = 20;
 
 fn init() -> Result<MainData, String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window(
+            "rust-sdl2 demo: Video",
+            GRID_X_SIZE * DOT_SIZE_IN_PXS,
+            GRID_Y_SIZE * DOT_SIZE_IN_PXS,
+        )
         .position_centered()
-        .opengl()
         .build()
         .map_err(|e| e.to_string())?;
 
-    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
     let event_pump = sdl_context.event_pump()?;
-    let data = MainData { event_pump, canvas };
+
+    let c = GameContext::default();
+
+    let data = MainData {
+        event_pump,
+        canvas,
+        c,
+    };
     Ok(data)
 }
 
 pub fn main() -> Result<(), String> {
     let mut d = init()?;
+
+    let mut i: u8 = 0;
 
     'running: loop {
         for event in d.event_pump.poll_iter() {
@@ -47,8 +92,11 @@ pub fn main() -> Result<(), String> {
             }
         }
 
+        i += 1;
+        d.canvas.set_draw_color(Color::RGB(255, i, 0));
         d.canvas.clear();
         d.canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
     }
 
     Ok(())
