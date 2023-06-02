2e74a55571975f8036af4832f0b692340b418b89
sir Random <RandomAmbersky@gmail.com>
2023-06-02 09:31:18 +0300

    temporary fixed

--- rs-core/src/pos2d.rs
+++ rs-core/src/pos2d.rs
@@ -5,17 +5,6 @@ pub struct Pos2D<T: UnsignedNum> {
     pub y: T,
 }
 
-pub struct MyStruct {
-    x: u32,
-    y: u32,
-}
-
-impl Default for MyStruct {
-    fn default() -> Self {
-        MyStruct { x: 10, y: 10 }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use crate::pos2d::{MyStruct, Pos2D};
