8c51c68776640666d4ec7ca2779c5a84f0f24e1e
sir Random <RandomAmbersky@gmail.com>
2023-06-02 09:20:19 +0300

    temporary fixed

--- git-set-remotes.sh
+++ git-set-remotes.sh
@@ -3,6 +3,6 @@
 # Credits: http://stackoverflow.com/a/750191
 
 git remote remove origin
-git remote add origin ssh://git@github.com/RandomAmbersky/test-go
+git remote add origin ssh://git@github.com/RandomAmbersky/ambernet-rust-engine
 git push --set-upstream origin master
 chmod 600 ~/.ssh/ed_random
