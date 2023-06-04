46a612d64623e6da2efbe4d7c4b52f03da8f6107
sir Random <RandomAmbersky@gmail.com>
2023-06-04 20:00:08 +0300

    some updates

--- rs-amberskynet/examples/03_texture2d/main.rs
+++ rs-amberskynet/examples/03_texture2d/main.rs
@@ -3,11 +3,47 @@ extern crate core;
 mod resource;
 mod view_2d;
 
+const GLOBAL_LOG_FILTER: log::LevelFilter = log::LevelFilter::Info;
+
 use crate::resource::{SHADER_SOURCE, TEXTURE_SOURCE};
 use crate::view_2d::View2D;
 use rs_amberskynet::{AsnContext, ExtHandlerTrait};
 use rs_gfx_core::AsnTextureTrait;
 use rs_gfx_wgpu::AsnTexture;
+
+fn init_log() {
+    let mut builder = fern::Dispatch::new();
+    let level_formatter;
+    #[cfg(target_arch = "wasm32")]
+    {
+        level_formatter = |level| level;
+        builder = builder.chain(fern::Output::call(console_log::log));
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        use fern::colors::{Color, ColoredLevelConfig};
+        let colors = ColoredLevelConfig::new()
+            .info(Color::Blue)
+            .debug(Color::Green);
+        level_formatter = move |level| colors.color(level);
+        builder = builder.chain(std::io::stdout());
+    }
+    builder
+        .level(GLOBAL_LOG_FILTER)
+        .level_for(module_path!(), log::LevelFilter::Debug)
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}:{}] {}",
+                chrono::Local::now().format("[%H:%M:%S]"),
+                level_formatter(record.level()),
+                record.target(),
+                record.line().unwrap_or_default(),
+                message
+            ))
+        })
+        .apply()
+        .unwrap();
+}
 
 struct Handler {
     view_2d: View2D,
@@ -21,20 +57,20 @@ impl Handler {
             .surface
             .get_capabilities(&ctx.gfx.adapter);
 
-        let surface_iter = surface_caps.formats.iter().copied();
-        for form in surface_iter {
-            println!("surface_format list: {:?}", form);
-        }
+        // let surface_iter = surface_caps.formats.iter(); //.copied();
+        // for form in surface_iter {
+        //     println!("surface_format list: {:?}", form);
+        // }
 
         let surface_format = surface_caps
             .formats
             .iter()
             .copied()
-            .find(|f| f.is_srgb())
-            // .next()
+            // .find(|f| f.is_srgb())
+            .next()
             .unwrap_or(surface_caps.formats[0]);
 
-        println!("surface_format: {:?}", surface_format);
+        // println!("surface_format: {:?}", surface_format);
 
         let shader = ctx
             .gfx
@@ -46,10 +82,10 @@ impl Handler {
 
         let texture = AsnTexture::from_raw_image(&ctx.gfx, TEXTURE_SOURCE).unwrap();
 
-        let texture_format = texture.texture.format();
-        println!("texture_format: {:?}", texture_format);
-        let update_format = texture_format.add_srgb_suffix();
-        println!("texture_format: {:?}", update_format);
+        // let texture_format = texture.texture.format();
+        // println!("texture_format: {:?}", texture_format);
+        // let update_format = texture_format.add_srgb_suffix();
+        // println!("texture_format: {:?}", update_format);
 
         let view_2d = View2D::new(&ctx.gfx.device, &texture, surface_format, &shader);
 
@@ -66,6 +102,7 @@ impl ExtHandlerTrait for Handler {
 }
 
 pub fn main() {
+    init_log();
     let (ctx, event_loop) = rs_amberskynet::init();
     let h = Handler::new(&ctx);
     rs_amberskynet::run(ctx, event_loop, h)
