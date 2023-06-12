bcfcb684c95712cdb7d300eaff25fb001ba002d7
sir Random <RandomAmbersky@gmail.com>
2023-06-12 13:10:49 +0300

    some updates

--- rs-gfx-core/src/texture_format.rs
+++ rs-gfx-core/src/texture_format.rs
@@ -1,9 +1,22 @@
 pub enum AsnTextureFormat {
-	#[allow(dead_code)]
-	Rgb8,
-	Rgb16,
-	Rgb32,
-	Rgba8,
-	Rgba16,
-	Rgba32,
+    #[allow(dead_code)]
+    Rgb8,
+    Rgb16,
+    Rgb32,
+    Rgba8,
+    Rgba16,
+    Rgba32,
+}
+
+impl AsnTextureFormat {
+    pub fn bytes_per_pixel(&self) -> u8 {
+        match self {
+            AsnTextureFormat::Rgb8 => 3,
+            AsnTextureFormat::Rgb16 => 3 * 2,
+            AsnTextureFormat::Rgb32 => 3 * 4,
+            AsnTextureFormat::Rgba8 => 4,
+            AsnTextureFormat::Rgba16 => 4 * 2,
+            AsnTextureFormat::Rgba32 => 4 * 4,
+        }
+    }
 }
