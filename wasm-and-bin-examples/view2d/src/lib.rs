ddd0a9741af3ac38fa6567a40254e6a963c30197
sir Random <RandomAmbersky@gmail.com>
2023-07-02 21:20:30 +0300

    some updates

--- wasm-and-bin-examples/view2d/src/lib.rs
+++ wasm-and-bin-examples/view2d/src/lib.rs
@@ -1,3 +1,25 @@
+use rs_amberskynet;
+use rs_amberskynet::{AsnContext, ExtHandlerTrait};
+use std::fmt::Error;
+
+struct Handler {}
+
+impl Handler {
+    fn new(ctx: &AsnContext) -> Result<Self, Error> {
+        let h = Handler {};
+        Ok(h)
+    }
+}
+
+impl ExtHandlerTrait for Handler {
+    fn draw(&mut self, e: &mut AsnContext) {}
+
+    fn update(&mut self, e: &mut AsnContext) {}
+}
+
 pub fn start() {
-    println!("Hello, world!");
+    let (ctx, event_loop) = rs_amberskynet::init();
+    if let Ok(_t) = Handler::new(&ctx) {
+        rs_amberskynet::run(ctx, event_loop, _t)
+    };
 }
