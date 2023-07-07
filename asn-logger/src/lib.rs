35b7362407d74a0b1bd27bd2010059d21a24b098
sir Random <RandomAmbersky@gmail.com>
2023-07-07 16:06:42 +0300

    some updates

--- asn-logger/src/lib.rs
+++ asn-logger/src/lib.rs
@@ -8,17 +8,11 @@ pub use log::{debug, info, log, trace, warn, Level};
 
 pub fn init_log(l: AsnLogLevel) {
     let log_level_filter = convert(l);
-    // cfg_if::cfg_if! {
-    //     if #[cfg(target_arch = "wasm32")] {
-    //         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    //         let level_option = log_level_filter.to_level();
-    //         if level_option.is_some() {
-    //             console_log::init_with_level(level_option.unwrap()).expect("Couldn't initialize logger");
-    //         }
-    //     } else {
+    cfg_if::cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        }
+    }
     log_level::init_log(log_level_filter);
     log!(target: "app_events", Level::Warn, "App warning");
-    // env_logger::init();
-    // }
-    // }
 }
