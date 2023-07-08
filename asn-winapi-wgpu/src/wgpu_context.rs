99bb53b873d91d708ce513e315b96fb476a7b41e
sir Random <RandomAmbersky@gmail.com>
2023-07-08 14:24:48 +0300

    some updates

--- asn-winapi-wgpu/src/wgpu_context.rs
+++ asn-winapi-wgpu/src/wgpu_context.rs
@@ -1,9 +1,12 @@
-use wgpu::{InstanceDescriptor, Surface};
+use wgpu::{Adapter, Device, InstanceDescriptor, Queue, Surface};
 use winit::window::Window;
 
 #[derive(Debug)]
 pub struct AsnWgpuContext {
     surface: Surface,
+    adapter: Adapter,
+    device: Device,
+    queue: Queue,
 }
 
 pub fn new_context(window: &Window) -> AsnWgpuContext {
@@ -14,5 +17,31 @@ pub fn new_context(window: &Window) -> AsnWgpuContext {
 
     let surface = unsafe { instance.create_surface(&window).unwrap() };
 
-    AsnWgpuContext { surface }
+    let adapter = instance
+        .enumerate_adapters(wgpu::Backends::all())
+        .find(|adapter| adapter.is_surface_supported(&surface))
+        .unwrap();
+
+    let (device, queue) = pollster::block_on(adapter.request_device(
+        &wgpu::DeviceDescriptor {
+            features: wgpu::Features::empty(),
+            // WebGL doesn't support all of wgpu's features, so if
+            // we're building for the web we'll have to disable some.
+            limits: if cfg!(target_arch = "wasm32") {
+                wgpu::Limits::downlevel_webgl2_defaults()
+            } else {
+                wgpu::Limits::default()
+            },
+            label: None,
+        },
+        None, // Trace path
+    ))
+    .unwrap();
+
+    AsnWgpuContext {
+        surface,
+        adapter,
+        device,
+        queue,
+    }
 }
