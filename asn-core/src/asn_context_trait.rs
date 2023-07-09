7ccda149e823b2e99e28ea559808bd97ecff21c0
sir Random <RandomAmbersky@gmail.com>
2023-07-09 19:41:42 +0300

    some updates

--- asn-core/src/asn_context_trait.rs
+++ asn-core/src/asn_context_trait.rs
@@ -1,10 +1,35 @@
 use crate::AsnWinapiTrait;
 
-pub trait AsnContextTrait<W>
+pub trait AsnContextTrait<'a, W>
 where
     W: AsnWinapiTrait,
 {
     fn is_need_exit(&self) -> bool;
     fn set_need_exit(&mut self);
-    fn winapi(&self) -> &'static W;
+    fn winapi(&self) -> &'a W;
+}
+
+pub struct AsnContext<'a, W>
+where
+    W: AsnWinapiTrait,
+{
+    is_need_exit: bool,
+    winapi: &'a W,
+}
+
+impl<'a, W> AsnContextTrait<'a, W> for AsnContext<'a, W>
+where
+    W: AsnWinapiTrait,
+{
+    fn is_need_exit(&self) -> bool {
+        self.is_need_exit
+    }
+
+    fn set_need_exit(&mut self) {
+        self.is_need_exit = true
+    }
+
+    fn winapi(&self) -> &'a W {
+        self.winapi
+    }
 }
