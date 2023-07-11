4d1306325cf8c0bfdb52be47f711f1b80cfc0c11
sir Random <RandomAmbersky@gmail.com>
2023-07-11 18:38:21 +0300

    some updates

--- asn-core/src/asn_context.rs
+++ asn-core/src/asn_context.rs
@@ -4,6 +4,7 @@ pub struct AsnContext<W>
 where
     W: AsnWinapiTrait,
 {
+    is_need_exit: bool,
     winapi: W,
 }
 
@@ -12,7 +13,16 @@ where
     W: AsnWinapiTrait,
 {
     pub fn new(winapi: W) -> Self {
-        Self { winapi }
+        Self {
+            winapi,
+            is_need_exit: false,
+        }
+    }
+    fn is_need_exit(&self) -> bool {
+        self.is_need_exit
+    }
+    fn set_need_exit(&mut self) {
+        self.is_need_exit = true;
     }
     pub fn get_winapi(&mut self) -> &mut W {
         &mut self.winapi
