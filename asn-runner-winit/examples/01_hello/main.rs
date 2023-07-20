422d365e7aaab4bbcbc0d2eb2ae7ba454e2b096d
sir Random <RandomAmbersky@gmail.com>
2023-07-20 19:25:03 +0300

    some updates

--- asn-runner-winit/examples/01_hello/main.rs
+++ asn-runner-winit/examples/01_hello/main.rs
@@ -1,13 +1,13 @@
 mod library;
 
 use crate::library::{get_context, get_handler};
-use asn_core::{AsnWinapiTrait, Size2D};
+
 use asn_logger::AsnLogLevel;
 
 fn main() {
     asn_logger::init_log(AsnLogLevel::Debug);
 
     let handler = get_handler();
-    let (runner, mut ctx) = get_context();
+    let (runner, ctx) = get_context();
     runner.run(ctx, handler);
 }
