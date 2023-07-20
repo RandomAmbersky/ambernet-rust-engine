3297e9507fa48e12cd9e6d52f217a0ca4853cbef
sir Random <RandomAmbersky@gmail.com>
2023-07-20 19:33:50 +0300

    some updates

--- asn-runner-winit/src/asn_scene/mod.rs
+++ asn-runner-winit/src/asn_scene/mod.rs
@@ -1,4 +1,25 @@
-pub mod asn_scene;
+use crate::asn_scene::asn_view2d::AsnView2d;
+use crate::asn_winapi::AsnWgpuWinApi;
+use asn_scene_view_core::{AsnSceneViewNodeTrait, AsnSceneViewTrait};
+
 pub mod asn_view2d;
 
-pub use asn_scene::AsnScene;
+pub struct AsnWgpuScene {}
+
+impl AsnWgpuScene {
+    pub fn new() -> Self {
+        AsnWgpuScene {}
+    }
+}
+
+impl AsnSceneViewTrait<AsnWgpuWinApi> for AsnWgpuScene {
+    type View2d = AsnView2d;
+
+    fn delete(_id: uuid::Uuid) {
+        todo!()
+    }
+
+    fn new_view2d(&mut self, api: &AsnWgpuWinApi) -> Self::View2d {
+        AsnView2d::new(api)
+    }
+}
