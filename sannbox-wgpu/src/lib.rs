855d39657b2e2106ffc149ad07e04f76e1eb3c66
sir Random <RandomAmbersky@gmail.com>
2023-07-26 07:40:52 +0300

    some updates

--- sannbox-wgpu/src/lib.rs
+++ sannbox-wgpu/src/lib.rs
@@ -1,8 +1,12 @@
 use crate::handler::Handler;
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
 mod engine;
 mod handler;
 
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 pub fn start() {
     println!("Hello, world!");
     let mut e = engine::Engine::new();
