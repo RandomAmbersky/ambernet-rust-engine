e2966c1ba11dfe5301cc78add7b981a8f88393f9
sir Random <RandomAmbersky@gmail.com>
2023-08-27 21:27:05 +0300

    some updates

--- asn-core/src/winapi/texture_format.rs
+++ asn-core/src/winapi/texture_format.rs
@@ -5,11 +5,13 @@ pub enum AsnTextureFormat {
     Rgba16,
     Rgba32,
     Bgra8UnormSrgb,
+    Rgba8UnormSrgb
 }
 
 impl AsnTextureFormat {
     pub fn bytes_per_pixel(&self) -> u32 {
         match self {
+            AsnTextureFormat::Rgba8UnormSrgb => 4,
             AsnTextureFormat::Bgra8UnormSrgb => 4,
             AsnTextureFormat::Rgba8 => 4,
             AsnTextureFormat::Rgba16 => 4 * 2,
--- sannbox-wgpu/src/engine/winapi/utils.rs
+++ sannbox-wgpu/src/engine/winapi/utils.rs
@@ -10,6 +10,7 @@ impl ToWgpuFormat for AsnTextureFormat {
     fn to_wgpu_format(self) -> wgpu::TextureFormat {
         match self {
             AsnTextureFormat::Rgba8 => wgpu::TextureFormat::Rgba8Unorm,
+            AsnTextureFormat::Rgba8UnormSrgb => wgpu::TextureFormat::Rgba8UnormSrgb,
             AsnTextureFormat::Rgba16 => wgpu::TextureFormat::Rgba16Unorm,
             AsnTextureFormat::Rgba32 => wgpu::TextureFormat::Rgba32Uint,
             AsnTextureFormat::Bgra8UnormSrgb => wgpu::TextureFormat::Bgra8UnormSrgb,
@@ -17,7 +18,8 @@ impl ToWgpuFormat for AsnTextureFormat {
     }
     fn get_from(f: wgpu::TextureFormat) -> Self {
         match f {
-            TextureFormat::Rgba8UnormSrgb => AsnTextureFormat::Rgba8,
+            TextureFormat::Rgba8UnormSrgb => AsnTextureFormat::Rgba8UnormSrgb,
+            TextureFormat::Rgba8Unorm => AsnTextureFormat::Rgba8,
             TextureFormat::Rgba16Unorm => AsnTextureFormat::Rgba8,
             TextureFormat::Rgba32Uint => AsnTextureFormat::Rgba8,
             TextureFormat::Bgra8UnormSrgb => AsnTextureFormat::Bgra8UnormSrgb,
