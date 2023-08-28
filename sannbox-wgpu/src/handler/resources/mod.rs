c43d2cd3fdeda69173e83f680df1e147c4708e4d
sir Random <RandomAmbersky@gmail.com>
2023-08-28 10:35:53 +0300

    some updates

--- sannbox-wgpu/src/handler/resources/mod.rs
+++ sannbox-wgpu/src/handler/resources/mod.rs
@@ -1,5 +1,5 @@
 pub const TEXTURE_QUAD_SOURCE: &[u8] = include_bytes!("flower-daisy-1100x733.jpg");
 
-pub const TEXTURE_TIILES_SOURCE: &[u8] = include_bytes!("tiles_mod.png");
+pub const TEXTURE_TIILES_SOURCE: &[u8] = include_bytes!("tiles.png");
 
-pub const MAP_TMX: &[u8] = include_bytes!("cell.tmx");
+pub const MAP_TMX: &[u8] = include_bytes!("map.tmx");
