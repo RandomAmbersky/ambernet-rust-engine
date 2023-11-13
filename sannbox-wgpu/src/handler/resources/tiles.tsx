7f148c1a9a1ee562862a56085de8e4e9680cd43d
sir Random <RandomAmbersky@gmail.com>
2023-11-13 10:45:51 +0300

    some updates

--- sannbox-wgpu/src/handler/resources/tiles.tsx
+++ sannbox-wgpu/src/handler/resources/tiles.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tiles_1" tilewidth="16" tileheight="16" tilecount="192" columns="16">
- <image source="./tiles.png" width="256" height="192"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles_1" tilewidth="16" tileheight="16" tilecount="192" columns="16">
+ <image source="tiles.png" width="256" height="192"/>
+ <tile id="45" type="Wanderer"/>
 </tileset>
