0c86af1c4b0d266bd6a047666928c45f1fc3aba3
sir Random <RandomAmbersky@gmail.com>
2023-11-23 11:27:13 +0300

    some updates

--- asn-decoder-tiled/src/tiles2.tsx
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="tiles_1" tilewidth="16" tileheight="16" tilecount="192" columns="16">
- <image source="tiles.png" width="256" height="192"/>
- <tile id="7" type="GreenContainer"/>
- <tile id="45" type="Wanderer"/>
-</tileset>
