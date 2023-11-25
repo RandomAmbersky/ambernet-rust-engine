8887142e1b5a6cd8fa2d49038f9665349d1e060c
sir Random <RandomAmbersky@gmail.com>
2023-11-25 15:43:22 +0300

    some updates

--- sannbox-wgpu/src/handler/mod.rs
+++ sannbox-wgpu/src/handler/mod.rs
@@ -298,27 +298,27 @@ impl TAsnHandler<Engine> for Handler {
 //     rng
 // }
 
-fn randomize_array(mut rng: SmallRng, a: &mut Array2D<u32, u8>) -> SmallRng {
-    let x = rng.gen_range(0..a.size.width);
-    let y = rng.gen_range(0..a.size.height);
-
-    let byte = rng.gen_range(0..3);
-
-    let index = y * a.size.width * 4 + x * 4 + byte;
-
-    let cell: u8 = rng.gen_range(0..255);
-    a.bytes[index as usize] = cell;
-    for x in 0..a.size.width {
-        for y in 0..a.size.height {
-            let index = ((y * a.size.width + x) * 4) as usize;
-            a.bytes[index] = rng.gen_range(0..128);
-            a.bytes[index + 1] = rng.gen_range(0..128);
-            a.bytes[index + 2] = rng.gen_range(0..128);
-            a.bytes[index + 3] = rng.gen_range(0..128);
-        }
-    }
-    rng
-}
+// fn randomize_array(mut rng: SmallRng, a: &mut Array2D<u32, u8>) -> SmallRng {
+//     let x = rng.gen_range(0..a.size.width);
+//     let y = rng.gen_range(0..a.size.height);
+//
+//     let byte = rng.gen_range(0..3);
+//
+//     let index = y * a.size.width * 4 + x * 4 + byte;
+//
+//     let cell: u8 = rng.gen_range(0..255);
+//     a.bytes[index as usize] = cell;
+//     for x in 0..a.size.width {
+//         for y in 0..a.size.height {
+//             let index = ((y * a.size.width + x) * 4) as usize;
+//             a.bytes[index] = rng.gen_range(0..128);
+//             a.bytes[index + 1] = rng.gen_range(0..128);
+//             a.bytes[index + 2] = rng.gen_range(0..128);
+//             a.bytes[index + 3] = rng.gen_range(0..128);
+//         }
+//     }
+//     rng
+// }
 
 fn fill_view(map: &AsnMap, start_pos: &Pos2D<u32>, view: &mut AsnNodeView2d) {
     for y in 0..view.get_size().height {
